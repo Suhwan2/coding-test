3be0f4080852710a58111239d3c45dba733e3257
suhwan <gkstnghks010@naver.com>
2022-09-20 14:44:54 +0900

    second

---
+++ 15649.py
@@ -0,0 +1,25 @@
+n,m=map(int,input().split())
+
+def prin():
+    for i in answer:
+        print(i,end=' ')
+    print()
+def dfs(cnt):
+    if cnt==m:
+        prin()
+    else:
+        for i in range(1,n+1):
+            if visited[i]:
+                continue
+            else:
+                visited[i]=1
+                answer.append(i)
+                dfs(cnt+1)
+                visited[i]=0
+                answer.pop()
+
+visited=[]
+answer=[]
+for i in range(n+1):
+    visited.append(0)
+dfs(0)
