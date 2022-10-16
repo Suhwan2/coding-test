d40fe8419b5a65bbd59b615ee429143986aaa816
suhwan <gkstnghks010@naver.com>
2022-10-16 23:15:20 +0900

    first

---
+++ 24444.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+n,m,r=map(int,sys.stdin.readline().split())
+visted=[0]*(n+1)
+e=[[] for _ in range(n+1)]
+result=[0]*(n+1)
+
+for _ in range(m):
+    a,b=map(int,sys.stdin.readline().split())
+    e[a].append(b)
+    e[b].append(a)
+
+for i in range(1,n+1):
+    e[i].sort()
+
+
+def bfs(r):
+    visted[r]=1
+    q=deque()
+    q.append(r)
+    cnt=1
+    result[r]=cnt
+    while q:
+        u=q.popleft()
+        for i in e[u]:
+            if visted[i]==0:
+                cnt+=1
+                result[i]=cnt
+                visted[i]=1
+                q.append(i)
+bfs(r)
+
+for i in range(1,n+1):
+    print(result[i])
---
+++ 24480.py
@@ -0,0 +1,29 @@
+from sys import stdin as st
+import sys
+sys.setrecursionlimit(1000000)
+n,m,r=map(int,st.readline().split())
+
+visited=[0]*(n+1)
+e=[[] for _ in range(n+1)]
+result=[0]*(n+1)
+
+for _ in range(m):
+    a,b=map(int,st.readline().split())
+    e[a].append(b)
+    e[b].append(a)
+for i in range(1,n+1):
+    e[i].sort(reverse=True)
+
+def dfs(r):
+    global cnt
+    cnt+=1
+    visited[r]=1
+    result[r]=cnt
+    for i in e[r]:
+        if visited[i]==0:
+            dfs(i)
+cnt=0
+dfs(r)
+
+for i in range(1,n+1):
+    print(result[i])
