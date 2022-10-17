dd30a8f784662402e142c5b9700c473064ae9df5
suhwan <gkstnghks010@naver.com>
2022-10-17 15:48:15 +0900

    second

--- 2178.py
+++ 2178.py
@@ -4,10 +4,7 @@ from collections import deque
 n,m=map(int,input().split())
 l=[list(map(int,sys.stdin.readline().strip())) for _ in range(n)]
 
-visited=[[0]*m for _ in range(n)]
-
 def bfs(x,y):
-    visited[x][y]=1
     q=deque()
     q.append((x,y))
 
@@ -19,8 +16,7 @@ def bfs(x,y):
             da=a+dx[i]
             db=b+dy[i]
             if da!=-1 and da!=n and db!=-1 and db!=m:
-                if l[da][db]!=0 and visited[da][db]==0:
-                    visited[da][db]=1
+                if l[da][db]==1:
                     q.append((da,db))
                     l[da][db]=l[a][b]+1
     
