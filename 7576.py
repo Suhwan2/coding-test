9d0ce90a9343d7bef9eea6c121f8e5b30fd46ed1
suhwan <gkstnghks010@naver.com>
2022-10-19 22:08:29 +0900

    second

--- 7576.py
+++ 7576.py
@@ -5,14 +5,15 @@ import copy
 m,n=map(int,input().split())
 t=[list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 
-def bfs(l,cnt):
-    if l:
-        q=deque(l)
+def bfs(l):
+    cnt=0
+    q=deque(l)
+    dx=[1,-1,0,0]
+    dy=[0,0,1,-1]
+    while q:
+        new_q=deque()
         cnt+=1
-        dx=[1,-1,0,0]
-        dy=[0,0,1,-1]
         while q:
-            new_q=deque()
             x,y=q.popleft()    
             for i in range(4):
                 xx=x+dx[i]
@@ -20,14 +21,24 @@ def bfs(l,cnt):
                 if xx>-1 and xx<n and yy>-1 and yy<m and t[xx][yy]==0:
                     t[xx][yy]=1
                     new_q.append((xx,yy))
-        bfs(new_q,cnt)       
-    else:
-        return cnt
+        q=copy.deepcopy(new_q)
+    return cnt-1
 l=deque()
 for i in range(n):
     for j in range(m):
         if t[i][j]==1:
             l.append((i,j))
 
-print(bfs(l,0))
-print(t)
+cnt=bfs(l)
+
+com=True
+for i in range(n):
+    for j in range(m):
+        if t[i][j]==0:
+            com=False
+            break
+
+if com:
+    print(cnt)
+else:
+    print('-1')    
