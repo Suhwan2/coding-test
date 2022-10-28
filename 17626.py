c1afa2877b1ed0a7744238f8b206f8063eaae0fc
suhwan <gkstnghks010@naver.com>
2022-10-28 14:51:22 +0900

    second

--- 17626.py
+++ 17626.py
@@ -1,14 +1,11 @@
 n=int(input())
 
-dp=[0]*(n+1)
+dp=[4]*(n+1)
 
+dp[0]=0
 dp[1]=1
 
 for i in range(2,n+1):
     for j in range(1,int(i**0.5)+1):
-        m=min(4,dp[i-j**2]+1)
-        if dp[i]:
-            dp[i]=min(dp[i],m)
-        else:
-            dp[i]=m
+        dp[i]=min(dp[i],dp[i-j**2]+1)
 print(dp[n])
