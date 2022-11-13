c1175162b33a796bdf5cff5e89cb329a57034600
suhwan <gkstnghks010@naver.com>
2022-11-13 20:34:13 +0900

    second

--- 12865.py
+++ 12865.py
@@ -2,8 +2,21 @@ import sys
 
 n,k=map(int,sys.stdin.readline().split())
 
-dp=[[0]]
+item=[[0,0]]
 for i in range(n):
-    w,v=map(int,sys.stdin.readline().split())
-    for j in range(w,k+1):
-        pass
+    a,b=map(int,sys.stdin.readline().split())
+    item.append((a,b))
+
+dp=[[0]*(k+1) for _ in range(n+1)]
+
+for i in range(1,n+1):
+    for j in range(1,k+1):
+        w=item[i][0]
+        v=item[i][1]
+
+        if j<w:
+            dp[i][j]=dp[i-1][j]
+        else:
+            dp[i][j]=max(dp[i-1][j],dp[i-1][j-w]+v)
+
+print(dp[n][k])
